ca56e12ea33f71c10f171a7056734c0a49385a78
shraddharocks6 <shraddha6pandey@gmail.com>
2020-07-22 19:54:01 +0530

    Sort of full frontend of the dashboard

--- models/cci.js
+++ models/cci.js
@@ -1,40 +1,47 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 var cciSchema = new mongoose.Schema({
-    cci_id : {
-        type : String,
-    },
-    address : {
-        type : String,
-    },
-    district : {
-        type : String,
-    },
-    state : {
-        type : String,  
-    },
-    pincode : {
-        type : Number, 
-    },
-    head_id : {
-        type : String,  //employee_id from the cciEmployeeSchema
-    },
-    attendence : [
+  cci_id: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  district: {
+    type: String,
+  },
+  state: {
+    type: String,
+  },
+  pincode: {
+    type: Number,
+  },
+  head_id: {
+    type: String, //employee_id from the cciEmployeeSchema
+  },
+  attendance: [
+    {
+      date: {
+        type: String,
+      },
+      data: [
         {
-            child_id : {
-                type : String
-            },
-            firstName  : {
-                type        : String,
-            },
-            lastName   : {
-                type        : String
-            },
-            date : {
-                type : String
-            },
-            present : {
-                type : boolean
-            }
-        }
-    ]
+          C_Id: {
+            type: String,
+          },
+          firstName: {
+            type: String,
+          },
+          middleName: {
+            type: String,
+          },
+          lastName: {
+            type: String,
+          },
+          present: {
+            type: Boolean,
+          },
+        },
+      ],
+    },
+  ],
 });
