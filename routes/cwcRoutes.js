8af1d242fc8af60c2e9fff69f875e8b03e83802e
shraddharocks6 <shraddha6pandey@gmail.com>
2020-07-25 13:38:21 +0530

    Everything before Tanisha Joined

--- routes/cwcRoutes.js
+++ routes/cwcRoutes.js
@@ -1,43 +1,29 @@
-const router  	    = require('express').Router();
-const CwcEmployee   = require('../models/cwcEmployee');
-    
+const router = require("express").Router();
+const CwcEmployee = require("../models/cwcEmployee");
 
-router.get("/cwc/dashboard/:employee_id", async function(req, res){
-    
-    const idToSearch = req.params.employee_id.substring(1);
+router.get("/cwc/dashboard/childRegistration/:employee_id", async function (
+  req,
+  res
+) {
+  const idToSearch = req.params.employee_id.substring(1);
 
-    const employee = await CwcEmployee.findOne({employee_id : idToSearch});
-    console.log("Employee found in CWC Route :");
-    console.log(employee);
-    res.render("CWC/dashboardHome.ejs", {employee:employee});
+  const employee = await CwcEmployee.findOne({ employee_id: idToSearch });
+  res.render("CWC/cwcdashboard-childRegistration.ejs", { employee: employee });
 });
 
+router.get("/cwc/dashboard/:employee_id", async function (req, res) {
+  const idToSearch = req.params.employee_id.substring(1);
 
-
-
-
-router.get("/cwc/dashboard/childRegistration/:employee_id", async function(req, res){
-    
-    const idToSearch = req.params.employee_id.substring(1);
-
-    const employee = await CwcEmployee.findOne({employee_id : idToSearch});
-    res.render("CWC/cwcdashboard-childRegistration.ejs", {employee:employee});
+  const employee = await CwcEmployee.findOne({ employee_id: idToSearch });
+  console.log("Employee found in CWC Route :");
+  console.log(employee);
+  res.render("CWC/dashboardHome.ejs", { employee: employee });
 });
 
+router.post("/addChild", function (req, res) {});
 
-router.post('/addChild', function(req, res){
-    
-});
+router.get("/registered/:id/:name", function (req, res) {});
 
-
-router.get('/registered/:id/:name',function(req, res){
-   
-});
-
-
-router.get('/details',function(req, res){
-       
-});
-
+router.get("/details", function (req, res) {});
 
 module.exports = router;
--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,16 +1,21 @@
-const router  	= require('express').Router();
+const { route } = require("./cwcRoutes");
 
-router.get("/", (req,res) => {
-    res.render('index.ejs');
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  res.render("index.ejs");
 });
 
-
-router.get("/loginOptions", (req,res) => {
-    res.render('loginOptions.ejs');
+router.get("/loginOptions", (req, res) => {
+  res.render("loginOptions.ejs");
 });
 
-router.get("/registerByAdminOptions", (req,res) => {
-    res.render('registerByAdminOptions.ejs');
+router.get("/registerByAdminOptions", (req, res) => {
+  res.render("registerByAdminOptions.ejs");
+});
+
+route.get("tanishaTest", (req, res) => {
+  res.render("Tanisha/");
 });
 
 module.exports = router;
