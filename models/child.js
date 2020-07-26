11c5b16bfdba7ddfba61ff1607336a2268b5a8ce
shraddharocks6 <shraddha6pandey@gmail.com>
2020-07-26 12:19:49 +0530

    After Tanisha's debugging

--- models/child.js
+++ models/child.js
@@ -1,55 +1,58 @@
 var mongoose = require("mongoose");
 
 var childSchema = new mongoose.Schema({
-    firstName  : {
-        type        : String,   //Raju
+  firstName: {
+    type: String, //Raju
+  },
+  middleName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  dateOfBirth: {
+    type: Date,
+  },
+  gender: {
+    type: String,
+  },
+  casteCategory: {
+    type: String,
+  },
+  aadharNumber: {
+    type: Number,
+  },
+  registrationDate: {
+    type: Date, //The current registration date
+  },
+  child_id: {
+    type: String,
+  },
+  cci_id: {
+    type: String, //cci_id from the cciSchema
+  },
+  cci_name: {
+    type: String,
+  },
+  cwc_id: {
+    type: String, //cwc_id from the cwcSchema
+  },
+
+  religion: {
+    type: String,
+  },
+  eligibilityStatus: {
+    type: String,
+  },
+  witness_id: {
+    //employee_id from the cwcEmployeeSchema
+    type: String,
+  },
+  guardian_id: [
+    {
+      type: String, //guardian_id from the guardianSchema
     },
-    middleName : {
-        type        : String
-    },
-    lastName   : {
-        type        : String
-    },
-    dateOfBirth : {
-        type : Date
-    },
-    registrationDate : {
-        type : Date          //The current registration date
-    },
-    child_id : {
-        type : String
-    },
-    cci_id : {
-        type : String       //cci_id from the cciSchema
-    },
-    cci_name    : {
-        type : String       
-    },
-    cwc_id : {
-        type : String       //cwc_id from the cwcSchema
-    },
-    gender      : {
-        type : String
-    },
-    aadharNumber : {
-        type : Number
-    },
-    casteCategory : {
-        type : String
-    },
-    religion : {
-        type : String
-    },
-    eligibilityStatus : {
-        type : String
-    },
-    witness_id : {          //employee_id from the cwcEmployeeSchema
-        type : String,      
-    },
-    guardian_id : [{
-        type : String,      //guardian_id from the guardianSchema
-    }]
+  ],
 });
 
 module.exports = mongoose.model("Child", childSchema);
-
