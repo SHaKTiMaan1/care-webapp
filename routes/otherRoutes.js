2adac0fcdd1d1384ebd2c4b361fa3e4f1bcb4253
Mridul <mridul0sharma@gmail.com>
2020-07-29 17:42:42 +0530

    State Official Registration complete and state official Login complete and State Dashboard Home Partially complete for current info

--- routes/otherRoutes.js
+++ routes/otherRoutes.js
@@ -2,14 +2,16 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const StateOfficial =require("../models/stateOfficial")
 const Cwc =require("../models/cwc")
+const Cci =require("../models/cci")
 
 
 
 router.get('/state/:official_id', async function(req,res){
   const stateOfficial = await StateOfficial.findOne({official_id:req.params.official_id})
   const cwc_count = await Cwc.countDocuments({state: stateOfficial.state})
+  const cci_count = await Cci.countDocuments({state: stateOfficial.state})
 
-  res.render("state/state-dashboard-home.ejs", {StateOfficial:stateOfficial, cwc_count:cwc_count});
+  res.render("state/state-dashboard-home.ejs", {StateOfficial:stateOfficial, cwc_count:cwc_count, cci_count:cci_count});
 });
 
 
