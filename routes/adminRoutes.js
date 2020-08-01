0b2ef293b177c500699c294f419974b3d3a6584f
shraddharocks6 <shraddha6pandey@gmail.com>
2020-08-01 09:36:08 +0530

    part 1 for eligibility criteria editing

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,11 +2,18 @@ const router = require("express").Router();
 const Admin = require("../models/admin");
 const bcrypt = require("bcryptjs");
 
-router.get("/admin/dashboard/:employee_id", async (req, res) => {
-  const idToSearch = req.params.employee_id;
+router.get(
+  "/admin/dashboard/eligibilityCritegia/:employee_id",
+  async (req, res) => {
+    const admin = await Admin.findOne({ employee_id: req.params.employee_id });
+    res.render("admin/eligibility-list-addition-criteria.ejs", {
+      admin: admin,
+    });
+  }
+);
 
-  const admin = await Admin.findOne({ employee_id: idToSearch });
-  console.log("I found you : " + admin);
+router.get("/admin/dashboard/:employee_id", async (req, res) => {
+  const admin = await Admin.findOne({ employee_id: req.params.employee_id });
   res.render("admin/adminDashboardHome.ejs", { admin: admin });
 });
 
