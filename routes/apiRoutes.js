faf19e8f2d12912a355a01e546ba6f3b10ca0e6f
Mridul <mridul0sharma@gmail.com>
2020-08-02 04:33:53 +0530

    adding the api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 
 const CciEmployee = require("../models/cciEmployee");
 const Cwc = require("../models/cwc");
@@ -6,8 +7,18 @@ const Cci = require("../models/cci");
 const Child = require("../models/child");
 
 
-router.get('/getChildInfo/:cci_id/:employeeEmail/:hashedPassword', async(req, res) =>{
-  const cci_employee = await CciEmployee.findOne({})
+router.get('/firstTimeLogin/:cci_id/:employeeEmail/:Password', async(req, res) =>{
+  const cci_employee = await CciEmployee.findOne({email: req.params.employeeEmail});
+
+  //checking if Password is valid
+  const isPasswordValid = await bcrypt.compare(
+    req.params.Password,
+    cci_employee.password
+  );
+
+  const child = await Child.find({cci_id:req.params.cci_id});
+  res.send(child);
+
 })
 
 
