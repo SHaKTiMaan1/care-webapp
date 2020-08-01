ce6abbc7d2ef72de6e96480f73873fc5b601bd81
shraddharocks6 <shraddha6pandey@gmail.com>
2020-08-02 05:19:27 +0530

    Making the APIs better

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,9 +6,10 @@ const Cwc = require("../models/cwc");
 const Cci = require("../models/cci");
 const Child = require("../models/child");
 
-
-router.get('/firstTimeLogin/:cci_id/:employeeEmail/:Password', async(req, res) =>{
-  const cci_employee = await CciEmployee.findOne({email: req.params.employeeEmail});
+router.get("/firstTimeLogin/:employeeEmail/:Password", async (req, res) => {
+  const cci_employee = await CciEmployee.findOne({
+    email: req.params.employeeEmail,
+  });
   console.log(hashedPassword);
   //checking if Password is valid
   const isPasswordValid = await bcrypt.compare(
@@ -16,16 +17,12 @@ router.get('/firstTimeLogin/:cci_id/:employeeEmail/:Password', async(req, res) =
     cci_employee.password
   );
 
-  const child = await Child.find({cci_id:cci_employee.cci_id});
-  if(isPasswordValid){
+  const child = await Child.find({ cci_id: cci_employee.cci_id });
+  if (isPasswordValid) {
     res.send(child);
-  }
-  else{
+  } else {
     res.sendStatus(401);
   }
-  
+});
 
-})
-
-
-module.exports = router
+module.exports = router;
