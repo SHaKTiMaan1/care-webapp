5e5729954ebde0b9d00f2ddaa67ae367fb8cce45
shraddharocks6 <shraddha6pandey@gmail.com>
2020-08-04 07:05:07 +0530

    Few changes in the UI

--- routes/dcpuRoutes.js
+++ routes/dcpuRoutes.js
@@ -7,28 +7,36 @@ const report = require("../models/report");
 const dcpuOfficer = require("../models/dcpuOfficer");
 
 router.get("/dcpu/dashboard/:employee_id", async (req, res) => {
-
-  const employee = await dcpuOfficer.findOne({employee_id : req.params.employee_id})
+  const employee = await dcpuOfficer.findOne({
+    employee_id: req.params.employee_id,
+  });
   // console.log(employee)
-  
-  const Report = await report.find({district:employee.district})
-  const cwc = await Cwc.findOne({district:employee.district})
-  idToSearch = cwc.cwc_id
-  const child_count = await Child.countDocuments({cwc_id:cwc.cwc_id})
-  console.log(Report)
-  res.render("dcpu/dcpu-dashboard-home.ejs", {employee: employee, report: Report, child_count:child_count})
+
+  const Report = await report.find({ district: employee.district });
+  const cwc = await Cwc.findOne({ district: employee.district });
+  idToSearch = cwc.cwc_id;
+  const child_count = await Child.countDocuments({ cwc_id: cwc.cwc_id });
+  console.log(Report);
+  res.render("dcpu/dcpu-dashboard-home.ejs", {
+    employee: employee,
+    report: Report,
+    child_count: child_count,
+  });
 });
 
+router.get("/dcpu/dashboard/:employee_id/seeReport/:messageId", async function (
+  req,
+  res
+) {
+  const employee = await dcpuOfficer.findOne({
+    employee_id: req.params.employee_id,
+  });
 
-router.get("/dcpu/dashboard/:employee_id/seeReport/:messageId", async function(req, res){
-
-  const employee = await dcpuOfficer.findOne({employee_id : req.params.employee_id})
-  
-
-  const foundReport = await report.findOne({_id: req.params.messageId})
-  res.render("Tanisha/report-dcpu-page.ejs",{ employee: employee,foundReport: foundReport})
-
-
-})
+  const foundReport = await report.findOne({ _id: req.params.messageId });
+  res.render("dcpu/report-dcpu-page.ejs", {
+    employee: employee,
+    foundReport: foundReport,
+  });
+});
 
 module.exports = router;
